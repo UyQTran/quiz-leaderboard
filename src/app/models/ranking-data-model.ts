52a026fe737720867e8bad8fccf38126e4cf22ab
Uy Tran <uqtontran@gmail.com>
2026-03-15 18:20:36 +0100

    update ranking model with lastUpdated instead of placement

--- src/app/models/ranking-data-model.ts
+++ src/app/models/ranking-data-model.ts
@@ -2,7 +2,6 @@ import { PlayerId } from './player-data-model';
 
 export type RankingId = string;
 export type Points = number;
-export type Placement = number;
 
 export interface RankingDataModel {
   rankingList: Ranking[]
@@ -12,5 +11,5 @@ export interface Ranking {
   rankingId: RankingId;
   playerId: PlayerId;
   points: Points;
-  placement: Placement;
+  lastUpdated: Date;
 }
