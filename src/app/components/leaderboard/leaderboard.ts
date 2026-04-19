920578664978afaab0e7fc3695a05b9e03d6a259
Uy Tran <uqtontran@gmail.com>
2026-04-19 19:59:43 +0200

    remove console log and sort by points

--- src/app/components/leaderboard/leaderboard.ts
+++ src/app/components/leaderboard/leaderboard.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core'
 import {
   MatCell,
   MatCellDef,
@@ -10,8 +10,8 @@ import {
   MatRow,
   MatRowDef,
   MatTable
-} from '@angular/material/table';
-import { DataFetcher } from '../../services/data-fetcher';
+} from '@angular/material/table'
+import { DataFetcher } from '../../services/data-fetcher'
 
 @Component({
   selector: 'app-leaderboard',
@@ -31,17 +31,15 @@ import { DataFetcher } from '../../services/data-fetcher';
   styleUrl: './leaderboard.scss',
 })
 export class Leaderboard {
-  playerData = inject(DataFetcher);
-  displayedColumns: string[] = ['name', 'points'];
+  playerData = inject(DataFetcher)
+  displayedColumns: string[] = ['name', 'points']
   dataSource = computed(() => {
-    console.log(this.playerData.playerBuffer().playerList)
-    console.log(this.playerData.rankingBuffer().rankingList)
     return this.playerData.playerBuffer().playerList.map((player) => {
       const ranking = this.playerData.rankingBuffer().rankingList.find((ranking) => ranking.playerId === player.playerId)
       if (!ranking) {
         return {name: player.playerName, points: 0}
       }
       return {name: player.playerName, points: ranking.points}
-    })
+    }).sort((a, b) => b.points - a.points)
   })
 }
